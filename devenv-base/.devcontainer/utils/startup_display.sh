0f981f29d865b3eba2d684d5139198dc38c62944
Yunho Ding <gurumindc123@gmail.com>
2025-03-09 15:12:02 +0800

    Add initial devcontainer setup with Dockerfile, CI/CD workflow, and Dependabot configuration

---
+++ devenv-base/.devcontainer/utils/startup_display.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+figlet 'CITS5206 - Group 11 Automated Setup dev container'
+echo 'This script will install all the necessary dependencies for the project to run.'
+echo 'Please wait for the installation to complete.'
+echo 'This may take a few minutes.'
+echo 'Thank you for your patience.'
+echo '---------------------------------------------------'
